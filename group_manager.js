4a77be846a2e60a33df9734c021a8578e8b7bdb1
Maxime Moreillon <m.moreillon@gmail.com>
2020-06-30 00:35:09 +0000

    routes cleanup

--- group_manager.js
+++ group_manager.js
@@ -35,35 +35,33 @@ const administrator_controller = require('./controllers/administrators.js')
 
 
 app.route('/groups')
-  .post(group_controller.create_group)
+  .post(group_controller.create_group) // Would require auth
 
 app.route('/groups/top_level')
-  .get(group_controller.get_top_level_groups)
+  .get(group_controller.get_top_level_groups) // No auth OK
 
 app.route('/groups/top_level/official')
-  .get(group_controller.get_top_level_official_groups)
+  .get(group_controller.get_top_level_official_groups) // No auth OK
 
 app.route('/groups/top_level/non_official')
-  .get(group_controller.get_top_level_non_official_groups)
+  .get(group_controller.get_top_level_non_official_groups) // No auth OK
 
 app.route('/groups/:group_id')
-  .get(group_controller.get_group)
-  .patch(group_controller.patch_group)
-  .delete(group_controller.delete_group)
+  .get(group_controller.get_group) // No auth OK
+  .patch(group_controller.patch_group) // Requires auth
+  .delete(group_controller.delete_group) // Requires auth
 
 // Subgroups
 app.route('/groups/:group_id/groups')
   .get(group_controller.get_groups_of_group)
+  .post(group_controller.add_group_to_group) // Requires auth
+  .delete(group_controller.remove_group_from_group) // Requires auth
 
 app.route('/groups/:group_id/groups/direct')
   .get(group_controller.get_groups_directly_belonging_to_group)
 
 app.route('/groups/:group_id/parent_groups')
   .get(group_controller.get_parent_groups_of_group)
-
-app.route('/groups/:group_id/groups')
-  .post(group_controller.add_group_to_group)
-  .delete(group_controller.remove_group_from_group)
 
 app.route('/groups/:group_id/groups/:subgroup_id')
   .post(group_controller.add_group_to_group)
@@ -100,30 +98,28 @@ app.route('/groups/:group_id/administrators/:administrator_id')
 app.route('/administrators/:administrator_id/groups')
   .get(administrator_controller.get_groups_of_administrator)
 
-app.route('/groups_of_administrator')
-  .get(administrator_controller.get_groups_of_administrator)
-
 
 
 ////////////
 // LEGACY //
 ////////////
 
+// Groups
 app.get('/group_by_id', group_controller.get_group)
 app.post('/create_group', group_controller.create_group)
 app.post('/delete_group', group_controller.delete_group)
-
-
-app.get('/groups_directly_belonging_to_group', group_controller.get_groups_directly_belonging_to_group)
 app.get('/parent_groups_of_group', group_controller.get_parent_groups_of_group)
 app.get('/top_level_groups', group_controller.get_top_level_groups)
 app.get('/top_level_groups/official', group_controller.get_top_level_official_groups)
 app.get('/top_level_groups/non_official', group_controller.get_top_level_non_official_groups)
-
-
-// TODO: Combine with below
 app.post('/join_group', group_controller.join_group)
 app.post('/leave_group', group_controller.leave_group)
+
+// Subgroups
+app.get('/groups_of_group', group_controller.get_groups_of_group)
+app.get('/groups_directly_belonging_to_group', group_controller.get_groups_directly_belonging_to_group)
+app.post('/add_group_to_group', group_controller.add_group_to_group)
+app.post('/remove_group_from_group', group_controller.remove_group_from_group)
 
 // Users
 app.get('/users_of_group', member_controller.get_members_of_group)
@@ -132,17 +128,11 @@ app.get('/users_with_no_group', member_controller.users_with_no_group)
 app.post('/remove_user_from_group', member_controller.remove_user_from_group)
 app.post('/add_user_to_group', member_controller.add_member_to_group)
 
-// Subgroups
-app.get('/groups_of_group', group_controller.get_groups_of_group)
-app.post('/add_group_to_group', group_controller.add_group_to_group)
-app.post('/remove_group_from_group', group_controller.remove_group_from_group)
-
 // Administrators
 app.get('/administrators_of_group', administrator_controller.get_administrators_of_group)
+app.get('groups_of_administrator', administrator_controller.get_groups_of_administrator)
 app.post('/make_user_administrator_of_group', administrator_controller.make_user_administrator_of_group)
 app.post('/remove_user_from_administrators', administrator_controller.remove_user_from_administrators)
-
-
 
 
 
