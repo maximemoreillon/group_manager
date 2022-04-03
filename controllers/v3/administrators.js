191b2f4843fd2e36af2f334d7edaad0f668fd937
Maxime Moreillon <m.moreillon@gmail.com>
2022-04-03 10:30:55 +0000

    shallow query for admin groups

--- controllers/v3/administrators.js
+++ controllers/v3/administrators.js
@@ -167,6 +167,7 @@ exports.get_groups_of_administrator = (req, res, next) => {
     ${user_query}
     WITH user
     OPTIONAL MATCH (user)<-[:ADMINISTRATED_BY]-(group:Group)
+    
     ${shallow === 'true' ? shallow_query : ''}
 
     WITH group as item
