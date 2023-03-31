9862029eddcece408159b0aaa9150ea47bb5b804
Maxime Moreillon <m.moreillon@gmail.com>
2023-03-31 20:43:11 +0900

    response if no record found

--- utils.js
+++ utils.js
@@ -1,5 +1,3 @@
-const createHttpError = require("http-errors")
-
 exports.get_current_user_id = (res) => {
   const current_user = res.locals.user
   const _id =
@@ -19,15 +17,9 @@ MATCH (user:User)
 ${exports.user_id_filter}
 `
 
-exports.current_user_query = `
-MATCH (current_user:User)
-WHERE current_user._id = $current_user_id
-`
+exports.current_user_query = `MATCH (current_user:User {_id: $current_user_id} )`
 
-exports.group_id_filter = `
-WHERE group._id = $group_id
-// OR id(group)=toInteger($group_id)
-`
+exports.group_id_filter = `WHERE group._id = $group_id`
 
 exports.group_query = `
 MATCH (group:Group)
@@ -72,7 +64,12 @@ RETURN
 exports.format_batched_response = (records) => {
   const record = records[0]
 
-  if (!record) throw createHttpError(404, "Query did not yield any match")
+  // TODO: This is a temporary fix for missing result record
+  if (!record)
+    return {
+      count: 0,
+      items: [],
+    }
 
   const items = record.get("batch")
   items.forEach((item) => {
