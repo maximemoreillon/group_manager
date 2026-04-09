a1b174b0d4a3e972fad149a5fdee487561bd05ac
Maxime Moreillon <m.moreillon@gmail.com>
2026-04-09 09:46:26 +0900

    API v1 and v2 now read-only

--- controllers/v2/administrators.ts
+++ controllers/v2/administrators.ts
@@ -39,119 +39,20 @@ export const get_administrators_of_group = async (
   }
 }
 
-export const make_user_administrator_of_group = async (
+export const make_user_administrator_of_group = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  // Route to leave a group
-
-  const { group_id } = req.params
-  if (!group_id) throw createHttpError(400, "Group ID not defined")
-
-  const user_id =
-    req.body.member_id ??
-    req.body.user_id ??
-    req.body.administrator_id ??
-    req.params.administrator_id
-
-  const current_user_id = get_current_user_id(req, res)
-
-  const session = driver.session()
-
-  const query = `
-    MATCH (current_user:User) WHERE $current_user_id IN ${getCypherUserIdentifiers(
-      "current_user"
-    )}
-    WITH current_user
-
-    MATCH (group:Group {_id: $group_id})
-    // Allow only group admin or super admin to delete a group
-    WHERE ( (group)-[:ADMINISTRATED_BY]->(current_user) OR current_user.isAdmin )
-
-    // Find the user
-    WITH group
-    MATCH (user:User) WHERE $user_id IN ${getCypherUserIdentifiers("user")}
-
-    // Merge relationship
-    MERGE (group)-[:ADMINISTRATED_BY]->(user)
-
-    // Return
-    RETURN user, group
-    `
-
-  const params = {
-    current_user_id,
-    user_id,
-    group_id,
-  }
-
-  try {
-    const { records } = await session.run(query, params)
-    if (!records.length)
-      throw createHttpError(400, `Error adding user to administrators`)
-    console.log(`User ${user_id} added to administrators of group ${group_id}`)
-    res.send(records[0].get("user"))
-  } catch (e) {
-    next(e)
-  } finally {
-    session.close()
-  }
+  res.status(410).send("Deprecated")
 }
 
-export const remove_user_from_administrators = async (
+export const remove_user_from_administrators = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  // Route to remove a user from the administrators of a group
-
-  const { group_id } = req.params
-  if (!group_id) throw createHttpError(400, "Group ID not defined")
-
-  const { administrator_id: user_id } = req.params
-  if (!user_id) throw createHttpError(400, "administrator_id ID not defined")
-
-  const session = driver.session()
-
-  const current_user_id = get_current_user_id(req, res)
-
-  const query = `
-    MATCH (current_user:User) WHERE $current_user_id IN ${getCypherUserIdentifiers(
-      "current_user"
-    )}
-
-    WITH current_user
-    MATCH (group:Group {_id: $group_id})
-    WHERE ( (group)-[:ADMINISTRATED_BY]->(current_user) OR current_user.isAdmin )
-
-    WITH group
-    MATCH (group)-[r:ADMINISTRATED_BY]->(user:User {_id: $user_id})
-
-    DELETE r
-
-    RETURN user, group
-    `
-
-  const params = {
-    current_user_id,
-    user_id,
-    group_id,
-  }
-
-  try {
-    const { records } = await session.run(query, params)
-    if (!records.length)
-      throw createHttpError(400, `Error removing from administrators`)
-    console.log(
-      `User ${user_id} removed from administrators of group ${group_id}`
-    )
-    res.send(records[0].get("user"))
-  } catch (e) {
-    next(e)
-  } finally {
-    session.close()
-  }
+  res.status(410).send("Deprecated")
 }
 
 export const get_groups_of_administrator = async (
