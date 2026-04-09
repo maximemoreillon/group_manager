011e5b9a1ecb7b9e1960a0434e42d13d551399ba
Maxime Moreillon <m.moreillon@gmail.com>
2026-04-09 15:21:52 +0900

    express-async-errors

--- index.ts
+++ index.ts
@@ -1,33 +1,34 @@
-import { version } from "./package.json"
-console.log(`= Group manager v${version} =`)
+import { version } from "./package.json";
+console.log(`= Group manager v${version} =`);
 
-import express from "express"
-import cors from "cors"
-import promBundle from "express-prom-bundle"
-import swaggerUi from "swagger-ui-express"
-import swaggerDocument from "./swagger-output.json"
-import { init as db_init } from "./db"
-import { errorHandler } from "./utils"
-import { APP_PORT } from "./config"
-import router from "./routes/"
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+import promBundle from "express-prom-bundle";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger-output.json";
+import { init as db_init } from "./db";
+import { errorHandler } from "./utils";
+import { APP_PORT } from "./config";
+import router from "./routes/";
 
-const promOptions = { includeMethod: true, includePath: true }
+const promOptions = { includeMethod: true, includePath: true };
 
-export const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(promBundle(promOptions))
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-app.use("/", router)
-app.use(errorHandler)
+export const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(promBundle(promOptions));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/", router);
+app.use(errorHandler);
 
 db_init()
   .then(() => {
     app.listen(APP_PORT, () => {
-      console.log(`[Express] listening on port ${APP_PORT}`)
-    })
+      console.log(`[Express] listening on port ${APP_PORT}`);
+    });
   })
   .catch((e) => {
-    console.error(e.message)
-    process.exit(1)
-  })
+    console.error(e.message);
+    process.exit(1);
+  });
